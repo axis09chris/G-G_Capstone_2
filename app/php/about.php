e573e28b1af2b3a28d1f1027a5105f175c22b973
Renzo Hernandez <112911770+hernandezrenzo@users.noreply.github.com>
2023-09-27 11:58:52 +0800

    update_partnerships

--- app/php/about.php
+++ app/php/about.php
@@ -11,7 +11,7 @@
     <section>
       <div class="about-img">
         <img src="../../img/gg-logo.png" alt="">
-        <p class="gg-title">The Place GianGaela's</p>
+        <p class="gg-title">The Place Gian & Gaela's</p>
         <hr>
       </div>
       <div class="about-img-2">
@@ -35,8 +35,30 @@
       <p class="checkpoint">Our owner, a graduate of BS Industrial Education major in Food Technology, possesses extensive knowledge and expertise in the field. With 36 units in Masters in Business Management and Masters in Public Administration, as well as two years of study in the College of Law, our owner's educational background and teaching experience in Business Entrepreneurship at the University of Batangas have greatly contributed to the success of our business.</p>
     </section>
   </main>
+
+  <section class="partnership-section">
+    <h2>Partnership</h2>
+    <hr>
+    <div class="container-partnership">
+      <div class="container-partner">
+        <img class="icon-partner" src="../../icons/jg-summit.png" alt="">
+        <img class="icon-partner" src="../../icons/urc.png" alt="">
+        <img class="icon-partner" src="../../icons/cemex-logo.png" alt="">
+      </div>
+      <div class="container-partner">
+        <img class="icon-partner" src="../../icons/goldengate.png" alt="">
+        <img class="icon-partner" src="../../icons/lpu1.png" alt="">
+        <img class="icon-partner" src="../../icons/ub.png" alt="">
+      </div>
+      <div class="container-partner">
+        <img class="icon-partner" src="../../icons/cipec.png" alt="">
+      </div>
+    </div>
+  </section>
+
   <section class="contact-us" id="contact-us">
     <h2>Contact Us</h2>
+    <hr>
     <div class="container-contact">
       <div class="container-form">
         <form id="contactForm">
@@ -53,16 +75,12 @@
             <input type="submit" class="btn btn-primary" value="Submit" name="submit">
           </div>
         </form>
-
-        <form action="registration.php" method="post">
-          
-      </form>
       </div>
       <div class="container-map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1937.8262683144526!2d121.070872261763!3d13.739474189256327!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd05024eed2a23%3A0x19d0a31ceec74e9!2sThe%20Place!5e0!3m2!1sen!2sph!4v1695610272180!5m2!1sen!2sph" width="450" height="320" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
-    </section>
+  </section>
   <footer>
     <?php include'../components/footer.php' ?>
   </footer>
