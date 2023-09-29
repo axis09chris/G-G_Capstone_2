50c8869e18ca4984e4457efce2c93a5db9cdb202
Renzo Hernandez <112911770+hernandezrenzo@users.noreply.github.com>
2023-09-30 06:54:04 +0800

    update

--- app/php/meals.php
+++ app/php/meals.php
@@ -5,10 +5,10 @@
 <body>
   <main>
     <div class="content-title">
-      <h1>Meals</h1>
+      <h1>Cuisine</h1>
     </div>
     <div class="meal-title">
-      <div><h2>The Place Signature Plated Menu</h2></div>
+      <div><h2>The Place Gian & Gaela's Signature Cuisine Menu</h2></div>
       <div>
         <p>
           Our dedication to culinary creativity and innovation enables us to update our plated buffet service on a regular basis, ensuring that you always have a wide selection of delicious catering menu packages to choose from, whether you're craving Filipino or foreign cuisine or something curated to your exact specifications.
@@ -20,43 +20,34 @@
       <div class="meal1">
         <div><img class="meal-image" src="../../img/filipino-plated.jpg" alt="filipino-plated"></div>
         <div>
-          <p class = "meal-name">Filipino Plated Menu</p>
+          <p class = "meal-name">Filipino Cuisine</p>
           <p>Filipino cuisine is sometimes recognized as the world's first genuinely multinational cuisine. The distinct flavor, originality, and diversity of Filipino food never cease to astonish visitors and natives alike. 
             <br>Filipino food is special because it incorporates numerous culinary traditions; for example, practically every dish reflects the influence of Spanish, Chinese, Indian, and Western cuisine.</p>
         </div>
       </div>
 
       <div class="meal1">
-        <div>
-          <p class = "meal-name">Italian Plated Menu</p>
-          <p>We invite you to savor the authentic flavors of Italy in every delectable dish. Our menu is a celebration of Italian culinary traditions, prepared with passion and using only the finest ingredients. Prepare to be transported to the sun-kissed landscapes of Italy with our carefully curated selection.</p>
-        </div>
-        <div><img class="meal-image" src="../../img/italian-plated.jpg" alt="filipino-plated"></div>
-      </div>
-
-      <div class="meal1">
         <div><img class="meal-image" src="../../img/american-plated.jpg" alt="filipino-plated"></div>
         <div>
-          <p class = "meal-name">American Plated Menu</p>
+          <p class = "meal-name">Batangueno's Best</p>
           <p>American plated dishes offer a culinary journey that celebrates the diverse flavors of American cuisine. These dishes blend traditional recipes with contemporary twists, showcasing regional ingredients and culinary heritage.</p>
         </div>
       </div>
 
       <div class="meal1">
         <div>
-          <p class = "meal-name">Pizza Plated Menu</p>
-          <p>Culinary artistry meets the timeless charm of pizza. Our menu features a delectable selection of handcrafted pizzas, thoughtfully curated to tantalize your taste buds. Indulge in our exquisite creations, each crafted with the finest ingredients and a touch of passion.</p>
+          <p class = "meal-name">Italian Cuisine</p>
+          <p>We invite you to savor the authentic flavors of Italy in every delectable dish. Our menu is a celebration of Italian culinary traditions, prepared with passion and using only the finest ingredients. Prepare to be transported to the sun-kissed landscapes of Italy with our carefully curated selection.</p>
         </div>
-        <div><img class="meal-image" src="../../img/pizza-plated.jpg" alt="filipino-plated"></div>
+        <div><img class="meal-image" src="../../img/italian-plated.jpg" alt="filipino-plated"></div>
       </div>
 
       <div class="meal1">
-        <div><img class="meal-image" src="../../img/pasta.jpg" alt="filipino-plated"></div>
         <div>
-          <p class = "meal-name">Pasta Plated Menu</p>
-          <p>Our pasta plated dishes offer a journey through the rich culinary heritage of Italy. Each plate is meticulously prepared to provide a memorable and indulgent pasta experience. Whether you're a fan of timeless classics or innovative creations, our pasta dishes showcase a harmonious blend of textures, aromas, and flavors. 
-            <br>With options ranging from creamy to tomato-based or herb-infused sauces, our pasta plates are expertly crafted to satisfy your cravings and transport you to the vibrant streets of Italy. Prepare to immerse yourself in the comfort and exquisite flavors of our pasta offerings.</p>
+          <p class = "meal-name">Pizza $ Pasta Buffet</p>
+          <p>Culinary artistry meets the timeless charm of pizza. Our menu features a delectable selection of handcrafted pizzas, thoughtfully curated to tantalize your taste buds. Indulge in our exquisite creations, each crafted with the finest ingredients and a touch of passion.</p>
         </div>
+        <div><img class="meal-image" src="../../img/pizza-plated.jpg" alt="filipino-plated"></div>
       </div>
     </div>
   </main>
