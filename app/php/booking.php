0f2d692324da3e514acaa54ec6c48ee543ff18f9
Renzo Hernandez <112911770+hernandezrenzo@users.noreply.github.com>
2023-10-05 21:06:37 +0800

    update booking

--- app/php/booking.php
+++ app/php/booking.php
@@ -8,12 +8,69 @@ if (!isset($_SESSION["user"])) {
 <head>
   <title>Booking Page</title>
   <?php include'../components/nav.php' ?>
+  <link rel="stylesheet" href="../css/booking.css?v=<?php echo time(); ?>">
 </head>
 <body>
+  <header class="content-title">
+    <h1>Booking</h1>
+  </header>
+
   <main>
-    Booking page
+    <div id="urModal" class="theModal">
+      <div class="the-modal-content">
+        <h2>Welcome to The Place Gian & Gaela's Catering Services</h2>
+        <div class="terms-intro">
+          <p>Welcome to our Catering Management and Reservation System! Before using our services, please read these Terms and Conditions carefully. By signing in and accessing our system, you agree to comply with and be bound by these terms and conditions.</p>
+        </div>
+        <p class="terms"><strong>1.</strong>&nbsp;Rates are subject to change without prior notice.</p>
+        <p class="terms"><strong>2.</strong>&nbsp;Whatever changes/addition or cancellation in the services covered by this contract is forfeited made at least two weeks before the occasion. Other optional items and services must be coordinated with the Account Executive in charge of the function at least a week prior to the event.</p>
+        <p class="terms"><strong>3.</strong>&nbsp;Catering Service shall end after the (4) four-hour service from the start of the event and additional fees per hour bases up to the time the last guest left the venue.</p>
+        <p class="terms"><strong>4.</strong>&nbsp;Down payment shall be forfeited if there will be cancellation. A <strong>NON-REFUNDABLE</strong> and <strong>NON-TRANSFERRABLE</strong> reservation fee of PHP 10,000.00 is <strong>REQUIRED</strong> and can be paid in cash, check or e-payments (Gcash, Paymaya, or Bank Transfer).</p>
+        <p class="terms"><strong>5.</strong>&nbsp;A <strong>50%</strong> deposit of the estimated package cost will be required upon signing of catering contract. Full payment shall be settled two weeks prior to the event. Incidental charges incurred during the event must be settled immediately after the function in cash. No checks will be accepted on the day of the function. Send bill arrangement will not be honored.</p>
+        <p class="terms"><strong>6.</strong>&nbsp;Full payment shall be settled immediately after the occasion or on the date of the occasion.</p>
+        <p class="terms"><strong>7.</strong>&nbsp;Replacing or removing amenities is not encouraged.</p>
+        <div class="terms-outro">
+          <p>By signing in and using our Catering Management and Reservation System, you acknowledge that you have read, understood, and agree to abide by these Terms and Conditions. If you do not agree with any of these terms, please refrain from using our services.</p>
+        </div>
+        <div>
+          <input type="checkbox" id="closeModalCheckbox" class="close-checkbox">
+          <label for="closeModalCheckbox" class="close-label">I agree to the terms & conditions</label>
+          <br>
+          <button id="thecloseModalBtn" class="theclose" onclick="closesModal()">Accept</button>
+        </div>
+      </div>
+    </div>
   </main>
+
   <footer>
     <?php include'../components/footer.php' ?>
   </footer>
+
+
+  <script>
+    // Get modal and close button elements
+    var modal = document.getElementById("urModal");
+    var closeModalBtn = document.getElementById("thecloseModalBtn");
+
+    // Function to open the modal
+    function openModal() {
+        modal.style.display = "block";
+    }
+
+    // Function to close the modal
+    function closesModal() {
+        modal.style.display = "none";
+    }
+
+    // Automatically open the modal when the page loads
+    window.addEventListener("load", openModal);
+
+    // Close the modal if the user clicks outside of it
+    window.addEventListener("click", function(event) {
+        if (event.target === modal) {
+            closesModal();
+        }
+    });
+
+  </script>
 </body>
--- app/php/terms.php
+++ app/php/terms.php
@@ -5,9 +5,10 @@
 </head>
 <body>
   <main>
-    <div class="content-title">
+    <header class="content-title">
       <h1>Terms and conditions</h1>
-    </div>
+    </header>
+    
     <div class="terms-intro">
       <p>Welcome to our Catering Management and Reservation System! Before using our services, please read these Terms and Conditions carefully. By signing in and accessing our system, you agree to comply with and be bound by these terms and conditions.</p>
     </div>
@@ -15,64 +16,66 @@
     <div class="container">
       <section class="terms">
         <h3>1. Acceptance of Terms</h3>
-        <p>- By accessing or using our Catering Management and Reservation System, you agree to these Terms and Conditions. If you do not agree with any part of these terms, please do not use our services.</p>
+        <p>•&nbsp;By accessing or using our Catering Management and Reservation System, you agree to these Terms and Conditions. If you do not agree with any part of these terms, please do not use our services.</p>
       </section>
 
       <section class="terms">
         <h3>2. Registration and Account Security</h3>
-        <p>- You must provide accurate and complete information during the registration process.</p>
-        <p>- You are responsible for maintaining the confidentiality of your account information and password.</p>
-        <p>- You are responsible for all activities that occur under your account.</p>
+        <p>•&nbsp;You must provide accurate and complete information during the registration process.</p>
+        <p>•&nbsp;You are responsible for maintaining the confidentiality of your account information and password.</p>
+        <p>•&nbsp;You are responsible for all activities that occur under your account.</p>
       </section>
 
       <section class="terms">
         <h3>3. Use of the system</h3>
-        <p>- Our Catering Management and Reservation System is intended for legitimate and lawful purposes only.</p>
-        <p>- You agree not to use our system for any illegal or unauthorized purpose.</p>
-        <p>- You will not use our system to transmit any malicious software or engage in any activity that could harm the system or other users.</p>
+        <p>•&nbsp;Our Catering Management and Reservation System is intended for legitimate and lawful purposes only.</p>
+        <p>•&nbsp;You agree not to use our system for any illegal or unauthorized purpose.</p>
+        <p>•&nbsp;You will not use our system to transmit any malicious software or engage in any activity that could harm the system or other users.</p>
       </section>
 
       <section class="terms">
         <h3>4. Reservation process</h3>
-        <p>- Our system allows you to make reservations for catering services.</p>
-        <p>- Reservations are subject to availability and confirmation by our staff.</p>
-        <p>- You may be required to provide payment information to secure a reservation.</p>
+        <p>•&nbsp;Our system allows you to make reservations for catering services.</p>
+        <p>•&nbsp;Reservations are subject to availability and confirmation by our staff.</p>
+        <p>•&nbsp;You may be required to provide payment information to secure a reservation.</p>
       </section>
 
       <section class="terms">
         <h3>5. Cancellation Policy</h3>
-        <p>- Cancellation policies may vary depending on the type of catering service and specific terms communicated during the reservation process..</p>
+        <p>•&nbsp;Cancellation policies may vary depending on the type of catering service and specific terms communicated during the reservation process..</p>
       </section>
 
       <section class="terms">
         <h3>6. Privacy policy</h3>
-        <p>- Your use of our system is also governed by our Privacy Policy, which outlines how we collect, use, and protect your personal information.</p>
+        <p>•&nbsp;Your use of our system is also governed by our Privacy Policy, which outlines how we collect, use, and protect your personal information.</p>
       </section>
 
       <section class="terms">
         <h3>7. Intellectual property</h3>
-        <p>- All content and materials provided through our system, including but not limited to text, images, and software, are protected by intellectual property rights and may not be used without our permission.</p>
+        <p>•&nbsp;All content and materials provided through our system, including but not limited to text, images, and software, are protected by intellectual property rights and may not be used without our permission.</p>
       </section>
 
       <section class="terms">
         <h3>8. Limitation and liability</h3>
-        <p>- We are not liable for any indirect, incidental, special, or consequential damages arising out of or in connection with the use of our system.</p>
+        <p>•&nbsp;We are not liable for any indirect, incidental, special, or consequential damages arising out of or in connection with the use of our system.</p>
       </section>
 
       <section class="terms">
         <h3>9. Termination</h3>
-        <p>- We reserve the right to terminate or suspend your account and access to our system at our discretion, without prior notice, for any reason, including if you violate these Terms and Conditions.</p>
+        <p>•&nbsp;We reserve the right to terminate or suspend your account and access to our system at our discretion, without prior notice, for any reason, including if you violate these Terms and Conditions.</p>
       </section>
 
       <section class="terms">
         <h3>10. Changes of terms</h3>
-        <p>- We may modify or update these Terms and Conditions from time to time. It is your responsibility to review these terms periodically for changes.</p>
+        <p>•&nbsp;We may modify or update these Terms and Conditions from time to time. It is your responsibility to review these terms periodically for changes.</p>
       </section>
     </div>
-    <div class="terms-outro">
-      <p>By signing in and using our Catering Management and Reservation System, you acknowledge that you have read, understood, and agree to abide by these Terms and Conditions. If you do not agree with any of these terms, please refrain from using our services.</p>
-    </div>
   </main>
+
+  <div class="terms-outro">
+    <p>By signing in and using our Catering Management and Reservation System, you acknowledge that you have read, understood, and agree to abide by these Terms and Conditions. If you do not agree with any of these terms, please refrain from using our services.</p>
+  </div>
+
   <footer>
     <?php include'../components/footer.php' ?>
   </footer>
